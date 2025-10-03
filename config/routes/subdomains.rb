1f85cb46e7c77b219fed147714579ca673056317
marcobeffa <marcobeffa@gmail.com>
2025-10-03 09:09:03 +0200

    fix

--- config/routes/subdomains.rb
+++ config/routes/subdomains.rb
@@ -1,20 +1,33 @@
 # config/routes/subdomains.rb
-Brand.includes(brand_services: :service_def).find_each do |brand|
-  # root dominio nudo
-  constraints host: /\A#{Regexp.escape(brand.host)}\z/ do
-    root to: "brands/home#show", as: "brand_root_#{brand.host.tr('.', '_')}"
-  end
+begin
+  # Evita di toccare il DB se non è pronto
+  if ActiveRecord::Base.connection_pool.connected? &&
+     Brand.table_exists? &&
+     BrandService.table_exists? &&
+     ServiceDef.table_exists?
 
-  # subdomain attivi = quelli dei service_defs associati al brand
-  subdomains = brand.brand_services.joins(:service_def)
-                     .pluck("service_defs.subdomain")
-                     .uniq
-                     .reject(&:blank?)
+    Brand.includes(brand_services: :service_def).find_each do |brand|
+      # root dominio nudo
+      constraints host: /\A#{Regexp.escape(brand.host)}\z/ do
+        root to: "brands/home#show", as: "brand_root_#{brand.host.tr('.', '_')}"
+      end
 
-  subdomains.each do |sub|
-    host = "#{sub}.#{brand.host}"
-    constraints host: /\A#{Regexp.escape(host)}\z/ do
-      root to: "services/hub#show", as: "root_#{host.tr('.', '_')}"
+      # subdomain attivi = quelli dei service_defs associati al brand
+      subdomains = brand.brand_services.joins(:service_def)
+                         .pluck("service_defs.subdomain")
+                         .uniq
+                         .reject(&:blank?)
+
+      subdomains.each do |sub|
+        host = "#{sub}.#{brand.host}"
+        constraints host: /\A#{Regexp.escape(host)}\z/ do
+          root to: "services/hub#show", as: "root_#{host.tr('.', '_')}"
+        end
+      end
     end
+  else
+    Rails.logger.warn "[ROUTES] Skip dynamic brand routes (tables missing)"
   end
+rescue ActiveRecord::NoDatabaseError, ActiveRecord::StatementInvalid => e
+  Rails.logger.warn "[ROUTES] Skip dynamic brand routes: #{e.class} #{e.message}"
 end
