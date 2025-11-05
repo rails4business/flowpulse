98e224cf0d63ef6d66f4ad4232b04f2154752526
marcobeffa <marcobeffa@gmail.com>
2025-11-05 14:23:44 +0100

    fix

--- db/migrate/20251013170959_create_users.rb
+++ db/migrate/20251013170959_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
+    return if table_exists?(:users)
     create_table :users do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
